557a96bf997d43a508c23f0ff2c003736f0928fb
N3dal <nedalxzo@gmail.com>
2023-03-12 19:51:44 +0200

    created defaults.py

---
+++ defaults.py
@@ -0,0 +1,14 @@
+"""
+    contain all the game defaults including the pictures,
+    and sounds;
+"""
+
+from pygame import image
+
+
+# window defaults;
+WINDOW_SIZE = [350, 600]
+BACKGROUND_COLOR = (158, 145, 232)
+
+
+BACKGROUND_DAY_IMAGE = image.load(r"./assets/sprites/background-day.png")
