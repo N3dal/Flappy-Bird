c0f123c3b3a8b982a306e2e80003d2561d84de36
N3dal <nedalxzo@gmail.com>
2023-03-13 22:01:02 +0200

    updated defaults.py

--- defaults.py
+++ defaults.py
@@ -17,3 +17,29 @@ BASE_IMAGE = image.load(r"./assets/sprites/base.png")
 GAME_OVER_IMAGE = image.load(r"./assets/sprites/gameover.png")
 START_MSG = image.load(r"./assets/sprites/message.png")
 GAME_ICON = image.load(r"./assets/favicon.ico")
+
+NUMBER_IMAGES = [image.load(
+    f"./assets/sprites/{num}.png") for num in range(10)]
+
+GREEN_PIPE_IMAGE = image.load(r"./assets/sprites/pipe-green.png")
+RED_PIPE_IMAGE = image.load(r"./assets/sprites/pipe-red.png")
+
+BLUE_BIRD_IMAGES = [
+    image.load(r"./assets/sprites/bluebird-downflap.png"),
+    image.load(r"./assets/sprites/bluebird-midflap.png"),
+    image.load(r"./assets/sprites/bluebird-upflap.png")
+]
+
+
+RED_BIRD_IMAGES = [
+    image.load(r"./assets/sprites/redbird-downflap.png"),
+    image.load(r"./assets/sprites/redbird-midflap.png"),
+    image.load(r"./assets/sprites/redbird-upflap.png")
+]
+
+
+YELLOW_BIRD_IMAGES = [
+    image.load(r"./assets/sprites/yellowbird-downflap.png"),
+    image.load(r"./assets/sprites/yellowbird-midflap.png"),
+    image.load(r"./assets/sprites/yellowbird-upflap.png")
+]
