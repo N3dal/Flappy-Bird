816008433eb0088f89e2faa9b812e893f92659de
N3dal <nedalxzo@gmail.com>
2023-03-13 21:46:05 +0200

    update defaults.py

--- defaults.py
+++ defaults.py
@@ -12,3 +12,8 @@ BACKGROUND_COLOR = (0, 0, 0)
 
 
 BACKGROUND_DAY_IMAGE = image.load(r"./assets/sprites/background-day.png")
+BACKGROUND_NIGHT_IMAGE = image.load(r"./assets/sprites/background-night.png")
+BASE_IMAGE = image.load(r"./assets/sprites/base.png")
+GAME_OVER_IMAGE = image.load(r"./assets/sprites/gameover.png")
+START_MSG = image.load(r"./assets/sprites/message.png")
+GAME_ICON = image.load(r"./assets/favicon.ico")
