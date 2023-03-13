fd367efa2395a85bdd7c7868d0613bba312e3ea5
N3dal <nedalxzo@gmail.com>
2023-03-13 21:47:04 +0200

    added an icon to the game

--- main.py
+++ main.py
@@ -18,25 +18,53 @@ from defaults import *
 pygame.init()
 
 
+class Pipe:
+    """"""
+    pass
+
+
+class Bird:
+    """"""
+    pass
+
+
+class Number:
+    """"""
+    pass
+
+
 def main():
 
     window = pygame.display.set_mode(WINDOW_SIZE)
 
-    var = 0
-    state = True
+    # setup the game icon;
+    pygame.display.set_icon(GAME_ICON)
+
+    base_img_x = 0
 
     # create the game main event loop;
     game_running_state = True
     while game_running_state:
+
+        window.fill(BACKGROUND_COLOR)
+        # set the background image;
+        window.blit(BACKGROUND_DAY_IMAGE, (0, 0))
+        window.blit(BASE_IMAGE, (base_img_x, 450))
+
+        if base_img_x == (WINDOW_SIZE[0] - BASE_IMAGE.get_width()):
+            window.blit(BASE_IMAGE, (base_img_x + 388, 450))
+            base_img_x = 0
+
+        base_img_x -= 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running_state = False
                 break
 
-        window.fill(BACKGROUND_COLOR)
-
         pygame.display.update()
+
+        pygame.time.wait(5)
 
     # end of the game;
     pygame.quit()
